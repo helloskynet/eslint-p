5cc04b9f111c31fad1ab6f5dd0ad7851d66b0b94
helloskynet <836886548@qq.com>
2023-12-05 22:03:14 +0800

    commet 增加说明

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const m = async (id) => {
+const m = asyncid => {
   return await Promise.resolve(id);
 };
 console.log(m);
 
 // 规则 arrow-parens 和 space-before-function-paren
 
+// Ctrl + S 或 Quick Fix --> fix all autofixable problems
 // 格式化之后
 
 // const m = asyncid => {

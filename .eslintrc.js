8994f7f55e6cff5c4d9658b15413416a321719c8
helloskynet <836886548@qq.com>
2023-12-05 22:26:48 +0800

    移除无关规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,36 +3,24 @@ module.exports = {
     browser: true,
     commonjs: true,
     es6: true,
-    node: true
+    node: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:@stylistic/recommended-extends',
-    'plugin:@stylistic/disable-legacy'
+    'plugin:@stylistic/disable-legacy',
   ],
   parser: '@babel/eslint-parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    sourceType: 'module'
-  },
   plugins: ['@stylistic'],
   rules: {
-    '@stylistic/max-len': ['error', { code: 100 }],
     '@stylistic/arrow-parens': ['error', 'as-needed'],
-    '@stylistic/comma-dangle': ['error', 'never'],
-    '@stylistic/operator-linebreak': ['error', 'after'],
-    '@stylistic/semi': ['error', 'always'],
-    '@stylistic/brace-style': ['error', '1tbs'],
-    '@stylistic/jsx-quotes': ['error', 'prefer-double'],
     '@stylistic/space-before-function-paren': [
       'error',
       {
         anonymous: 'never',
         named: 'never',
-        asyncArrow: 'never'
-      }
-    ]
-  }
-};
+        asyncArrow: 'never',
+      },
+    ],
+  },
+}
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const m = async (id) => {
-  return await Promise.resolve(id);
-};
-console.log(m);
+  return await Promise.resolve(id)
+}
+console.log(m)
 
 // 规则 arrow-parens 和 space-before-function-paren
 
-// Ctrl + S 或 Quick Fix --> fix all autofixable problems
-// 格式化之后
+// VScode 安装ESlint插件之后
+// 使用 Quick Fix --> fix all autofixable problems
+// 或者开启保存自动修复之后，保存也会触发
+
+// 在控制台直接执行 eslint --fix index.js 不会出现这个问题
+
+// 格式化之后结果
 
 // const m = asyncid => {
 //   return await Promise.resolve(id);
